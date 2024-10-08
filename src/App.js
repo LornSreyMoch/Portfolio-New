4aabde7dfeb79b53290bc8f210efc88f902002d8
MochNoch <sreymoch.lorn@institute.pse.ngo>
2024-10-08 14:14:20 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <h1 className='bg-cyan-100'>Welcome to your portfolio!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
